347ba4b23423650d2d2d4d4eea7c753fb3e3d5ac
HoPollo <hopollotv@gmail.com>
2019-10-10 06:29:28 +0200

    added autofetch version

--- controllers/aboutController.js
+++ controllers/aboutController.js
@@ -1,7 +1,17 @@
 'use strict';
 
+const fs = require('fs');
+const record = require('../lib/utils/record/record');
+
 const aboutPath = require('path').resolve('./lib/about.html');
 
 exports.index = (req, res) => {
   res.sendFile(aboutPath);
 };
+
+exports.version_get = (req, res) => {
+  fs.readFile(appRoot + '/version', 'utf8', (err, data) => {
+    if (err) record(err);
+    res.json(data);
+  })
+}
