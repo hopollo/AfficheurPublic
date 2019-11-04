974c0b0cd857953e55655caafab75e0fec2121ee
hopollo <hopollotv@gmail.com>
2019-11-04 13:23:07 +0100

    new admin menu feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -7,6 +7,22 @@ exports.index = (req, res) => {
   res.sendFile(appRoot + '/lib/admin.html');
 };
 
+exports.menu_get = (req, res, next) => {
+  const menu = 
+  `<div class="tab">
+    <button onclick="openTab(event, 'Users')">Users</button>
+    <button onclick="openTab(event, 'Views')">Views</button>
+    <button onclick="openTab(event, 'Logs')">Logs</button>
+    <button onclick="openTab(event, 'Terminal')">Terminal</button>
+
+    <div id="Users" class="tabContent"></div>
+    <div id="Views" class="tabContent"></div>
+    <div id="Logs" class="tabContent"></div>
+    <div id="Terminal" class="tabContent"></div>
+  </div>`;
+  res.send(menu);
+}
+
 exports.users_get = (req, res, next) => {
   console.log('users_get')
 }
@@ -23,9 +39,48 @@ exports.user_delete = (req, res, next) => {
   console.log('user_delte');
 };
 
+exports.terminal_get = (req, res) => {
+  console.log('terminal_get');
+  res.json('Not Implemented yet.');
+}
+
 exports.logs_get = (req, res) => {
   console.log('logs_get');
-  res.send('THERE IS SERVERS LOGS');
+  try {
+    fs.readdir(appRoot + '/logs', (err, logs) => {
+      if (err) throw err;
+      if (!logs) return;
+      let logsList = [];
+      let i = logs.length;
+      logs.forEach(log => {
+        if (!log.includes('log')) return i--;
+        new Promise((resolve, reject) => {
+          fs.stat(appRoot + '/logs/' + log, (err, stat) => {
+            if (err) reject(err);
+
+            function formatFileSize(bytes, decimalPoint) {
+              if(bytes == 0) return '0 Bytes';
+              var k = 1000,
+                  dm = decimalPoint || 1,
+                  sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+                  i = Math.floor(Math.log(bytes) / Math.log(k));
+              return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+            }
+
+            const logElement = {title: log.split('.')[0], 
+                                 size: formatFileSize(stat.size),
+                                 date: stat.birthtime}
+            resolve(logElement);
+          });
+        })
+        .then(logElement => {
+          i--;
+          logsList.push(logElement);
+          if (i === 0) return res.json(logsList);
+        });
+      });
+    });
+  } catch (err) { record(err); }
 };
 
 exports.views_get = (req, res) => {
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -9,18 +9,21 @@ const authLite = require('../lib/utils/database/dbManagerLite');
 module.exports = router;
 
 router.get('/', admin_controller.index);
+
+if (enableMongoDB) router.get('/menu', auth.auth, admin_controller.menu_get);
+router.get('/menu', authLite.auth, admin_controller.menu_get);
+
 router.get('/logs', admin_controller.logs_get);
 router.get('/views', admin_controller.views_get);
+router.get('/terminal', admin_controller.terminal_get);
 
-if (enableMongoDB) router.get('/users', auth.auth, auth.getEntries, admin_controller.users_get);
-router.get('/users', authLite.auth, authLite.getEntries, admin_controller.users_get);
-
+if (enableMongoDB) router.get('/users', auth.getEntries, admin_controller.users_get);
+router.get('/users', authLite.getEntries, admin_controller.users_get);
 
 if (enableMongoDB) router.post('/user', auth.createEntry, admin_controller.user_post);
 router.post('/user', authLite.createEntry, admin_controller.user_post);
 
 //router.update('/user', auth.updateEntry, admin_controller.user_update);
-
 
 if (enableMongoDB) router.delete('/user', auth.dropEntry, admin_controller.user_delete);
 router.delete('/user', authLite.dropEntry, admin_controller.user_delete);
